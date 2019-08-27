8a91449d306ebbccdcf5b976c19b396e750b6570
Tim Loden <tim.m.loden@gmail.com>
2019-08-27 09:51:45 -0700

    added plugin updater

--- caweb-custom-modules.php
+++ caweb-custom-modules.php
@@ -25,16 +25,29 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with CAWeb Custom Modules. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
 */
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/timloden/CAWeb-custom-modules',
+    __FILE__,
+    'caweb-custom-modules'
+);
+
+//Optional: If you're using a private repository, specify the access token like this:
+//$myUpdateChecker->setAuthentication('your-token-here');
+
+//Optional: Set the branch that contains the stable release.
+//$myUpdateChecker->setBranch('stable-branch-name');
 
 
-if ( ! function_exists( 'cacm_initialize_extension' ) ):
-/**
- * Creates the extension's main class instance.
- *
- * @since 1.0.0
- */
-function cacm_initialize_extension() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/CawebCustomModules.php';
-}
-add_action( 'divi_extensions_init', 'cacm_initialize_extension' );
+if (! function_exists('cacm_initialize_extension') ) :
+    /**
+     * Creates the extension's main class instance.
+     *
+     * @since 1.0.0
+     */
+    function cacm_initialize_extension()
+    {
+        include_once plugin_dir_path(__FILE__) . 'includes/CawebCustomModules.php';
+    }
+    add_action('divi_extensions_init', 'cacm_initialize_extension');
 endif;
